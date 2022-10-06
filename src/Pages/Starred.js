5619437d67749c993da215fd5681f8b2d6e4ae93
Azad Ansari <79094390+AZAD-CYBER@users.noreply.github.com>
2022-10-06 21:27:12 +0530

    read_file

--- src/Pages/Starred.js
+++ src/Pages/Starred.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MainPageLayout from "../components/MainPageLayout";
 function Starred() {
-  return <MainPageLayout>this is Starred</MainPageLayout>;
+  return <MainPageLayout> Starred</MainPageLayout>;
 }
 
 export default Starred;
