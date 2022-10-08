bf3e9aa30895203b9ea24a77c9d962e29d48aef1
Azad Ansari <79094390+AZAD-CYBER@users.noreply.github.com>
2022-10-08 10:40:52 +0530

    Show Actor Componenets added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
-import Navs from "./components/Navs";
 import Home from "./Pages/Home";
 import Starred from "./Pages/Starred";
 
